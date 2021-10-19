4a9ea5ef2be0df5aecf3de0e265f4a4652d6aa7e
Vishesh singhal <78434665+ItsVishesh@users.noreply.github.com>
2021-10-19 18:23:41 +0530

    Add files via upload

---
+++ PRINT() STATEMENT.py
@@ -0,0 +1,4 @@
+print("my name is vishesh singhal","i am pursuing btech")
+print("I am a programmer",end=" , ")
+print(" and i like python programming")
+print("I am a programmer",end=" and i like python programming")
---
+++ variable and datatype.py
@@ -0,0 +1,17 @@
+vishesh="40"
+demo=55.5
+demo2=40
+print(type(vishesh))
+demo3=type(demo)
+print(demo3)
+print(type(demo2))
+
+'''var1="it's a string"
+var2=5
+print(var1+var2)'''
+
+var1="My Name is "
+var2="Vishesh"
+var3=var1+var2+" & I am a Good Boy."
+print(var1+var2)
+print(var3)
