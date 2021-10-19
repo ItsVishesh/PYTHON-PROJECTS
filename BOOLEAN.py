0c15b3723f60913e341673c73787632c743bf6d2
Vishesh singhal <78434665+ItsVishesh@users.noreply.github.com>
2021-10-19 18:19:45 +0530

    Add files via upload

---
+++ BOOLEAN.py
@@ -0,0 +1,21 @@
+print(bool (10 == 20))
+print(bool ('cats' != 'dogs'))
+
+a = 200
+b = 33
+
+if b>a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+print(bool('hello'))
+print(bool(15))
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(''))
+print(bool())
+print(bool([]))
+print(bool({}))
