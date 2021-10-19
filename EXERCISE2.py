4940c004d0815426fcd33ce6dbc961f52706ed16
Vishesh singhal <78434665+ItsVishesh@users.noreply.github.com>
2021-10-19 18:20:58 +0530

    Add files via upload

---
+++ EXERCISE2.py
@@ -0,0 +1,13 @@
+print("Enter your 1st Number : ")
+n1 = input()
+print("Enter your 2nd Number : ")
+n2 = input()
+print("sum of these two numbers is ",int(n1)+int(n2))
+
+a = int(input("Enter Your 1st Number : "))
+b = int(input("Enter Your 2nd Number : "))
+print(a,"+",b,"=",a+b)
+
+c = int(input("c = "))
+d = int(input("d = "))
+print("c + d =",c+d)
---
+++ INPUT() FUNCTION.py
@@ -0,0 +1,6 @@
+# INPUT FUNCTION IN PYTHON
+print("Enter your Name : ")
+name = input()  # it will take input from the user
+print("Your Name is",name)  # it will shows the name
+xyz = input("Enter your  age : ")
+print("Your age is ",xyz)
